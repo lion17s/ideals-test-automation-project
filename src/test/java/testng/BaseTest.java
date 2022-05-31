0d5c4e5726c2c923f313b57048ac4bbdc4d0dbef
Serhii Sheptytskyi <lion17s@Serhiis-MacBook-Pro.local>
2022-05-31 21:21:35 +0300

    Update BaseTest class

--- src/test/java/testng/BaseTest.java
+++ src/test/java/testng/BaseTest.java
@@ -1,8 +1,33 @@
 package testng;
 
-import org.testng.annotations.Listeners;
+import org.testng.annotations.*;
+import ta.core.driver.DriverFactory;
+import ta.core.env.Environment;
 import ta.core.testng.listeners.TestListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Listeners(TestListener.class)
 public abstract class BaseTest {
+
+    @Parameters("env")
+    @BeforeSuite(alwaysRun = true)
+    public void setupEnvironment(@Optional(value = "desktop.chrome") String env) {
+        Environment.setEnvironment(env);
+    }
+
+    @BeforeMethod
+    public void setupDriver() {
+        String driver = Environment.getCurrentEnvironment().get("driver");
+        Map<String, Object> capabilities = Environment.getCurrentEnvironment().getOrDefault(driver, new HashMap<>());
+        DriverFactory.setDriver(driver, capabilities);
+
+    }
+
+    @AfterMethod
+    public void quitDriver() {
+        DriverFactory.quitDriver();
+    }
+
 }
