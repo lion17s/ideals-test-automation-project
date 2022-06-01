f18e73f3d14902dc21fef6f3a8919f5d9082a50f
lion17s <serhii.sheptytskyi@gmail.com>
2022-06-01 13:31:38 +0300

    Remove unused code

--- src/main/java/ta/core/ui/BaseUIElement.java
+++ src/main/java/ta/core/ui/BaseUIElement.java
@@ -1,26 +1,17 @@
 package ta.core.ui;
 
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import ta.core.driver.DriverFactory;
 import ta.core.env.Environment;
-
-import java.time.Duration;
 
 public abstract class BaseUIElement {
 
     private static final int WAIT_TIME_SECONDS = Environment.getOrDefault("driverWait", 3);
 
     public BaseUIElement() {
-        if (DriverFactory.getDriver() instanceof AppiumDriver) {
-            PageFactory.initElements(new AppiumFieldDecorator(
-                    DriverFactory.getDriver(), Duration.ofSeconds(WAIT_TIME_SECONDS)), this);
-        } else {
-            PageFactory.initElements(new AjaxElementLocatorFactory(
-                    DriverFactory.getDriver(), WAIT_TIME_SECONDS), this);
-        }
+        PageFactory.initElements(new AjaxElementLocatorFactory(
+                DriverFactory.getDriver(), WAIT_TIME_SECONDS), this);
     }
 
 }
